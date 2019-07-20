b10c12c8caff06cd38b58a23dfc69be32ee9a59f
Claus Holst <clh@visiolink.com>
2019-07-20 15:52:43 +0200

    App updater

--- android/app/src/main/kotlin/dk/youtec/drchannels/MainActivity.kt
+++ android/app/src/main/kotlin/dk/youtec/drchannels/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity: FlutterActivity() {
     GeneratedPluginRegistrant.registerWith(this)
 
     if(savedInstanceState == null) {
-      updateApp(this@MainActivity,
+      updateApp(this,
               BuildConfig.VERSION_CODE,
               "https://www.dropbox.com/s/9nn23pjnocityi8/drchannels-flutter.json?dl=1",
               "https://www.dropbox.com/s/k0beuis03cf4zao/drchannels-flutter.apk?dl=1",
