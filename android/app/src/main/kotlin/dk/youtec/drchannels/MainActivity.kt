54f09d0d7ed0f6979c9e22036ddb0df79f63d8f5
Claus Holst <clh@visiolink.com>
2019-07-20 15:48:29 +0200

    App updater

--- android/app/src/main/kotlin/dk/youtec/drchannels/MainActivity.kt
+++ android/app/src/main/kotlin/dk/youtec/drchannels/MainActivity.kt
@@ -1,6 +1,7 @@
 package dk.youtec.drchannels
 
 import android.os.Bundle
+import dk.youtec.appupdater.updateApp
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -9,5 +10,13 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    if(savedInstanceState == null) {
+      updateApp(this@MainActivity,
+              BuildConfig.VERSION_CODE,
+              "https://www.dropbox.com/s/9nn23pjnocityi8/drchannels-flutter.json?dl=1",
+              "https://www.dropbox.com/s/k0beuis03cf4zao/drchannels-flutter.apk?dl=1",
+              "")
+    }
   }
 }
